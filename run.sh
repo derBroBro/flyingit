8dfb16313031f3292895bd0d452c27a988bcd413
Malte Brodersen <malte.brodersen@exoit.de>
2017-05-22 19:27:02 +0200

    Update run.sh
    Update to matching fly version

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 fly -t main login -c $CC_URL -u $CC_USER -p $CC_PASS -n main
+fly -t main sync
 git clone $GIT_URL git_repo
 fly set-pipeline -t main --config git_repo/$CC_CONFIG_FILE --pipeline $CC_PIPELINE --non-interactive
